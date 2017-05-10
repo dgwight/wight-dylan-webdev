239289cdc0b9f61ae0c09b039c0d445faa3e1388
Dylan Wight <dylangwight@gmail.com>
2017-05-10 14:17:18 -0400

    setup mlab

--- test/app.js
+++ test/app.js
@@ -6,12 +6,11 @@ module.exports = function(app)
 
     var connectionString = 'mongodb://127.0.0.1:27017/test';
 
-    if(process.env.MLAB_USERNAME) {
-        connectionString = process.env.MLAB_USERNAME + ":" +
-            process.env.MLAB_PASSWORD + "@" +
-            process.env.MLAB_HOST + ':' +
-            process.env.MLAB_PORT + '/' +
-            process.env.MLAB_APP_NAME;
+    if(process.env.MLAB_USERNAME_WEBDEV) {
+	var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+    	var password = process.env.MLAB_PASSWORD_WEBDEV;
+    	connectionString = 'mongodb://' + username + ':' + password;
+    	connectionString += '@ds137101.mlab.com:37101/heroku_dzpfc8qg'; // user yours	
     }
 
     var mongoose = require("mongoose");
