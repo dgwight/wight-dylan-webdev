0ffb746579120c2781dcc488898216ce5eedbbd2
Dylan Wight <dylangwight@gmail.com>
2017-05-23 16:00:33 -0400

    WebAppMaker

--- public/assignment/app.js
+++ public/assignment/app.js
@@ -1,3 +1,10 @@
 /**
  * Created by DylanWight on 5/23/17.
  */
+var app = angular.module("WebAppMaker", []);
+
+app.controller("WebAppMakerController", WebAppMakerController);
+
+function WebAppMakerController($scope) {
+    $scope.hello = "Hello World from AngularJS";
+}
