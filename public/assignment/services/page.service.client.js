22fb8682d14b1a0a54b227b78cd62edec5d0313a
Dylan Wight <dylangwight@gmail.com>
2017-05-23 18:02:20 -0400

    PageService

--- public/assignment/services/page.service.client.js
+++ public/assignment/services/page.service.client.js
@@ -1,3 +1,62 @@
 /**
  * Created by DylanWight on 5/23/17.
  */
+(function () {
+    angular
+        .module("WebAppMaker")
+        .factory("PageService", PageService);
+    function PageService() {
+        var pages = [
+            {"_id": "321", "name": "Post 1", "websiteId": "456", "description": "Lorem"},
+            {"_id": "432", "name": "Post 2", "websiteId": "456", "description": "Lorem"},
+            {"_id": "543", "name": "Post 3", "websiteId": "456", "description": "Lorem"}
+        ];
+        var api = {
+            "createPage": createPage,
+            "findPageByWebsiteId": findPageByWebsiteId,
+            "findPageById": findPageById,
+            "updatePage": updatePage,
+            "deletePage": deletePage
+        };
+        return api;
+
+        function createPage(websiteId, page) {
+            page.websiteId = websiteId;
+            pages.append(page);
+        }
+
+        function findPageByWebsiteId(websiteId) {
+            var websitePages = [];
+            for (page in pages) {
+                if (page.websiteId === websiteId)
+                    websitePages.append(website);
+            }
+            return websitePages;
+        }
+
+        function findPageById(pageId) {
+            for (page in pages) {
+                if (page._id === pageId)
+                    return page;
+            }
+            return null;
+        }
+
+        function updatePage(pageId, page) {
+            for (p in pages) {
+                if (p._id === page) {
+                    p = page;
+                    return;
+                }
+            }
+        }
+
+        function deletePage(pageId) {
+            for (var i = 0; i < pages.length - 1; i++) {
+                if (pages[i]._id === pageId) {
+                    websites.splice(i, 1);
+                }
+            }
+        }
+    }
+})();
--- public/assignment/services/website.service.client.js
+++ public/assignment/services/website.service.client.js
@@ -1,27 +1,26 @@
 /**
  * Created by DylanWight on 5/23/17.
  */
-(function() {
+(function () {
     angular
         .module("WebAppMaker")
         .factory("WebsiteService", WebsiteService);
     function WebsiteService() {
         var websites = [
-                { "_id": "123", "name": "Facebook",    "developerId": "456", "description": "Lorem" },
-                { "_id": "234", "name": "Tweeter",     "developerId": "456", "description": "Lorem" },
-                { "_id": "456", "name": "Gizmodo",     "developerId": "456", "description": "Lorem" },
-                { "_id": "890", "name": "Go",          "developerId": "123", "description": "Lorem" },
-                { "_id": "567", "name": "Tic Tac Toe", "developerId": "123", "description": "Lorem" },
-                { "_id": "678", "name": "Checkers",    "developerId": "123", "description": "Lorem" },
-                { "_id": "789", "name": "Chess",       "developerId": "234", "description": "Lorem" }
-            ]
-        ;
+            {"_id": "123", "name": "Facebook", "developerId": "456", "description": "Lorem"},
+            {"_id": "234", "name": "Tweeter", "developerId": "456", "description": "Lorem"},
+            {"_id": "456", "name": "Gizmodo", "developerId": "456", "description": "Lorem"},
+            {"_id": "890", "name": "Go", "developerId": "123", "description": "Lorem"},
+            {"_id": "567", "name": "Tic Tac Toe", "developerId": "123", "description": "Lorem"},
+            {"_id": "678", "name": "Checkers", "developerId": "123", "description": "Lorem"},
+            {"_id": "789", "name": "Chess", "developerId": "234", "description": "Lorem"}
+        ];
         var api = {
-            "createWebsite"   : createWebsite,
-            "findWebsitesByUser" : findWebsitesByUser,
-            "findWebsiteById" : findWebsiteById,
-            "updateWebsite" : updateWebsite,
-            "deleteWebsite" : deleteWebsite
+            "createWebsite": createWebsite,
+            "findWebsitesByUser": findWebsitesByUser,
+            "findWebsiteById": findWebsiteById,
+            "updateWebsite": updateWebsite,
+            "deleteWebsite": deleteWebsite
         };
         return api;
 
@@ -29,6 +28,7 @@
             website.developerId = userid;
             websites.append(website);
         }
+
         function findWebsitesByUser(userId) {
             var userWebsites = [];
             for (website in websites) {
@@ -37,6 +37,7 @@
             }
             return userWebsites;
         }
+
         function findWebsiteById(websiteId) {
             for (website in websites) {
                 if (website._id === websiteId)
@@ -44,6 +45,7 @@
             }
             return null;
         }
+
         function updateWebsite(websiteId, website) {
             for (ws in websites) {
                 if (ws._id === websiteId) {
@@ -52,9 +54,10 @@
                 }
             }
         }
+
         function deleteWebsite(websiteId) {
-            for(var i = 0; i < websites.length - 1; i++) {
-                if(websites[i]._id === websiteId) {
+            for (var i = 0; i < websites.length - 1; i++) {
+                if (websites[i]._id === websiteId) {
                     websites.splice(i, 1);
                 }
             }
