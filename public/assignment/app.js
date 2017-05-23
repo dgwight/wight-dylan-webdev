e4fe550d38083c245d6d499194daf78e2fd8a841
Dylan Wight <dylangwight@gmail.com>
2017-05-23 17:08:25 -0400

    loads login page

--- public/assignment/app.js
+++ public/assignment/app.js
@@ -1,10 +1,7 @@
 /**
  * Created by DylanWight on 5/23/17.
  */
-var app = angular.module("WebAppMaker", []);
-
-app.controller("WebAppMakerController", WebAppMakerController);
-
-function WebAppMakerController($scope) {
-    $scope.hello = "Hello World from AngularJS";
-}
+(function(){
+    angular
+        .module("WebAppMaker", ["ngRoute"]);
+})();
--- public/assignment/config.js
+++ public/assignment/config.js
@@ -1,53 +1,62 @@
 /**
  * Created by DylanWight on 5/23/17.
  */
-(function() {
+(function () {
     angular
         .module("WebAppMaker")
         .config(Config);
     function Config($routeProvider) {
         $routeProvider
-            .when("/register", {
-                templateUrl: "/views/user/register.view.client.html"
+            .when("/", {
+                templateUrl: "views/user/login.view.client.html"
+            })
+            .when("default", {
+                templateUrl: "views/user/login.view.client.html"
             })
             .when("/login", {
-                templateUrl: "/views/user/login.view.client.html"
+                templateUrl: "views/user/login.view.client.html"
             })
-            .when("/profile", {
-                templateUrl: "/views/user/profile.view.client.html"
+            .when("/register", {
+                templateUrl: "views/user/register.view.client.html"
             })
-            .when("/website-edit", {
-                templateUrl: "/views/website/website-edit.view.client.html"
+            .when("/user/:uid", {
+                templateUrl: "views/user/profile.view.client.html"
             })
-            .when("/website-list", {
-                templateUrl: "/views/website/website-list.view.client.html"
+            .when("/user/:uid/website/:wid", {
+                templateUrl: "views/website/website-edit.view.client.html"
             })
-            .when("/website-new", {
-                templateUrl: "/views/website/website-new.view.client.html"
+            .when("/user/:uid/website", {
+                templateUrl: "views/website/website-list.view.client.html"
             })
-            .when("/page-edit", {
+            .when("/user/:uid/website/new", {
+                templateUrl: "views/website/website-new.view.client.html"
+            })
+            .when("/user/:uid/website/:wid/page/:pid", {
                 templateUrl: "/views/page/page-edit.view.client.html"
             })
-            .when("/page-list", {
-                templateUrl: "/views/page/page-list.view.client.html"
+            .when("/user/:uid/website/:wid/page", {
+                templateUrl: "views/page/page-list.view.client.html"
             })
-            .when("/page-new", {
-                templateUrl: "/views/page/page-new.view.client.html"
+            .when("/user/:uid/website/:wid/page/new", {
+                templateUrl: "views/page/page-new.view.client.html"
             })
-            .when("/widget-choose", {
-                templateUrl: "/views/widget/widget-choose.view.client.html"
+            .when("/user/:uid/website/:wid/page/:pid/widget/new", {
+                templateUrl: "views/widget/widget-chooser.view.client.html"
+            })
+            .when("/user/:uid/website/:wid/page/:pid/widget", {
+                templateUrl: "views/widget/widget-list.view.client.html"
+            })
+            .when("/user/:uid/website/:wid/page/:pid/widget/:wgid", {
+                templateUrl: "views/widget/widget-edit.view.client.html"
             })
             .when("/widget-heading", {
-                templateUrl: "/views/widget/widget-heading.view.client.html"
+                templateUrl: "views/widget/widget-heading.view.client.html"
             })
             .when("/widget-image", {
-                templateUrl: "/views/widget/widget-image.view.client.html"
-            })
-            .when("/widget-list", {
-                templateUrl: "/views/widget/widget-list.view.client.html"
+                templateUrl: "views/widget/widget-image.view.client.html"
             })
             .when("/widget-youtube", {
-                templateUrl: "/views/widget/widget-youtube.view.client.html"
+                templateUrl: "views/widget/widget-youtube.view.client.html"
             })
     }
 })();
