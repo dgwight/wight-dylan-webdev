600f979abd4ede7478c11c2bc5a51e9d01884d84
Dylan Wight <dylangwight@gmail.com>
2017-06-05 13:53:31 -0400

    touch-punch

--- public/assignment/directives/wd-dragable.directive.client.js
+++ public/assignment/directives/wd-dragable.directive.client.js
@@ -13,27 +13,12 @@
             var initial;
             $(element).sortable({
                 update: function(event, ui) {
-                    console.log('update: '+ui.item.index(), attributes["wdDraggable"]);
                     var pageId = attributes["wdDraggable"];
                     WidgetService.reorder(pageId, initial, ui.item.index());
                 },
                 start: function(event, ui) {
-                    console.log('start: ' + ui.item.index());
                     initial = ui.item.index();
-                },
-                stop: function(event, ui) {
-                    console.log('stop: ' + ui.item.index());
-                },
-                end: function (e) {
-                    console.log('end: ');
-                    e.preventDefault();
-                    $('.dropHint').remove();
-                    var itemMoved = angular.copy($scope.content[e.oldIndex]);
-                    scope.content.splice(e.oldIndex, 1);
-                    scope.content.splice(e.newIndex, 0, itemMoved);
-                    scope.$apply();
                 }
-
             })
         }
 
